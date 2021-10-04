585e4decd0cccefbfaabdd110a0c52347b91f4c5
Administrator <admin@example.com>
2021-10-04 19:39:32 +0200

    Update output description

--- output.tf
+++ output.tf
@@ -1,13 +1,13 @@
 output "bootstrap_server_root_password" {
+  description = "BootVM generated password"
   value = random_password.kickoffvm_password.result
 }
 output "bootstrap_server_ip" {
+  description = "BootVM ip"
   value = var.tkg_bootvm.ip
 }
 
 output "kubctl_config" {
+  description = "Kubeconf for MGMT cluster content"
   value = data.sshcommand_command.get_kubectl.result
 }
-
-
-
